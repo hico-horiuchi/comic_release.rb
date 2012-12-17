12d662e857ff7963fc3e5dc1e2c71c6730b19f07
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2012-12-18 08:38:34 +0900

    tmpは不要だと思う

--- comic_release.rb
+++ comic_release.rb
@@ -8,15 +8,15 @@ require './just_kana.rb'
 require './comic_list.rb'
 
 CSV.foreach(ARGV[0]) { |raw|
-  tmpComic = ComicList.new(raw[0], raw[1])
+  comic = ComicList.new(raw[0], raw[1])
 
-  print(ljust_kana(tmpComic.title, 25).tosjis)
+  print(ljust_kana(comic.title, 25).tosjis)
 
-  printf("%02d", tmpComic.volume)
+  printf("%02d", comic.volume)
   print("巻  ".tosjis)
 
-  if tmpComic.is_release_decided
-    print(tmpComic.release_date.strftime("%m/%d") + "\n")
+  if comic.is_release_decided
+    print(comic.release_date.strftime("%m/%d") + "\n")
   else
     puts "未定".tosjis
   end
