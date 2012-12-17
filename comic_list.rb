c817e28cf36eaddd40484671fba376ed6c5802d7
Akihiko Horiuchi <12ff5b8@gmail.com>
2012-12-17 15:11:13 +0900

    [comic_list] ver01 新規追加
    クラス Comic を継承するサブクラス ComicList の定義。

---
+++ comic_list.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+Encoding.default_external = 'UTF-8'
+
+require 'cgi'
+require 'open-uri'
+require './comic.rb'
+
+class ComicList < Comic
+  # タイトルと巻数からURLを生成
+  def encodeURL(name, num)
+    # URLエンコード
+    return 'http://comiclist.jp/index.php?p=s&mode=ss&keyword=' + CGI.escape(@name.toutf8) + '+' + @num.to_s
+  end
+
+  # URLから発売日を取得
+  def getReleaseDate(url)
+    releaseDate = Date::today
+
+    # HTMLファイル取得
+    open (url) { |f|
+      # 1行ずつ読み込み、正規表現で発売日を抽出
+      f.read =~ /<td class="list-line list-day">(.*?)<\/td>/
+      if $1 != nil then # 発売日が決定の場合
+        releaseDate = Date.strptime($1, "%m/%d")
+        @flag = "release"
+      end
+    }
+    return releaseDate
+  end
+end
