8f0c690a506acf37ac2d18adaeafa2d080ea5b80
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-01-24 15:13:30 +0900

    発売日が未定の漫画は表示しない

--- comic_release.rb
+++ comic_release.rb
@@ -31,7 +31,9 @@ shelf.import(ARGV[0])               # 漫画リスト(CSV)の読み込み
 puts "Getting comics information."
 shelf.resolve_books_attributes      # 発売日を取得
 shelf.each do |comic|               # リストの漫画について
-  puts comic.to_s                   # タイトル,巻数,発売日を出力
+  if comic.is_release_decided       # 発売日が決定している場合
+      puts comic.to_s               # タイトル,巻数,発売日を出力
+  end
 end
 
 # 処理時間の計測終了と結果の出力
